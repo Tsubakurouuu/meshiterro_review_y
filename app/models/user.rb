3f5b3d2d8a65eba3fcbce36df981d0c4fa2b507d
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-07 04:45:35 +0000

    フォロー機能モデルコントローラ完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  has_many :relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  
+  has_many :followings, through: :relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :following
 
   has_one_attached :profile_image
 
@@ -16,5 +22,17 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user_id)
+    relationships.create(follower_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(follower_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :edit, :update, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
