7cc96866bbe4e034db1c6b1f030ccaeda02a4e5f
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-11 02:45:58 +0000

    post_commentにバリデーションと非同期機能の追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,17 @@
 class PostCommentsController < ApplicationController
   def create
     @post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_image_id = @post_image.id
-    comment.save
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_image_id = @post_image.id
+    unless @post_comment.save
+      render 'error'
+    end
   end
 
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.find_by(post_image_id: @post_image.id)
-    comment.destroy
+    @post_comment = current_user.post_comments.find_by(post_image_id: @post_image.id)
+    @post_comment.destroy
   end
 
   def post_comment_params
